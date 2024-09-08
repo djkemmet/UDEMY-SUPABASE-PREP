d40bdb1c9b627dbe58e4b143ae643913542a8fd7
DJ Kemmet <kemmetdaniel@gmail.com>
2024-09-07 21:55:08 -0500

    Section One: Intialize Supabase in our Project

---
+++ flutter_superbase/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  //initalize Supabase
+  String supabaseURL = dotenv.env['SUPABASE_URL']!;
+  String supabaseKey = dotenv.env["SUPABASE_KEY"]!;
+
+  await Supabase.initialize(url: supabaseURL, anonKey: supabaseKey);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
